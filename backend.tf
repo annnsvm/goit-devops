81512d658f965f261406db240ca10c1c5636ac00
Anna Samoilova <121616319+annnsvm@users.noreply.github.com>
2025-12-15 22:28:20 +0100

    lesson-7: helm chart for Django project

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket-001001-daria-hk"
+    key            = "lesson-5/terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+  }
+}
